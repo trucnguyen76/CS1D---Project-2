4097bdba4d3a28173ae1f31fce5ec2bc7141774c
Erick Lo <fermo5303@hotmail.com>
2015-04-30 18:41:49 -0700

    Revert "gitignore"
    This reverts commit 069dd3e644d451deacd8c10f179bb68445bdb419.

---
+++ build-Stadium_Trip-Desktop_Qt_5_4_0_MSVC2013_64bit-Debug/debug/moc_mainwindow.cpp
@@ -0,0 +1,148 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.4.0)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../Stadium_Trip/mainwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.4.0. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+struct qt_meta_stringdata_MainWindow_t {
+    QByteArrayData data[11];
+    char stringdata[237];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MainWindow_t, stringdata) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
+QT_MOC_LITERAL(1, 11, 18), // "on_quitBtn_clicked"
+QT_MOC_LITERAL(2, 30, 0), // ""
+QT_MOC_LITERAL(3, 31, 26), // "on_planVacationBtn_clicked"
+QT_MOC_LITERAL(4, 58, 32), // "on_modifySouvenirListBtn_clicked"
+QT_MOC_LITERAL(5, 91, 31), // "on_modifyStadiumListBtn_clicked"
+QT_MOC_LITERAL(6, 123, 19), // "on_logInBtn_clicked"
+QT_MOC_LITERAL(7, 143, 21), // "on_pushButton_clicked"
+QT_MOC_LITERAL(8, 165, 23), // "on_pushButton_2_clicked"
+QT_MOC_LITERAL(9, 189, 23), // "on_pushButton_3_clicked"
+QT_MOC_LITERAL(10, 213, 23) // "on_pushButton_4_clicked"
+
+    },
+    "MainWindow\0on_quitBtn_clicked\0\0"
+    "on_planVacationBtn_clicked\0"
+    "on_modifySouvenirListBtn_clicked\0"
+    "on_modifyStadiumListBtn_clicked\0"
+    "on_logInBtn_clicked\0on_pushButton_clicked\0"
+    "on_pushButton_2_clicked\0on_pushButton_3_clicked\0"
+    "on_pushButton_4_clicked"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       9,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,   59,    2, 0x08 /* Private */,
+       3,    0,   60,    2, 0x08 /* Private */,
+       4,    0,   61,    2, 0x08 /* Private */,
+       5,    0,   62,    2, 0x08 /* Private */,
+       6,    0,   63,    2, 0x08 /* Private */,
+       7,    0,   64,    2, 0x08 /* Private */,
+       8,    0,   65,    2, 0x08 /* Private */,
+       9,    0,   66,    2, 0x08 /* Private */,
+      10,    0,   67,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        MainWindow *_t = static_cast<MainWindow *>(_o);
+        switch (_id) {
+        case 0: _t->on_quitBtn_clicked(); break;
+        case 1: _t->on_planVacationBtn_clicked(); break;
+        case 2: _t->on_modifySouvenirListBtn_clicked(); break;
+        case 3: _t->on_modifyStadiumListBtn_clicked(); break;
+        case 4: _t->on_logInBtn_clicked(); break;
+        case 5: _t->on_pushButton_clicked(); break;
+        case 6: _t->on_pushButton_2_clicked(); break;
+        case 7: _t->on_pushButton_3_clicked(); break;
+        case 8: _t->on_pushButton_4_clicked(); break;
+        default: ;
+        }
+    }
+    Q_UNUSED(_a);
+}
+
+const QMetaObject MainWindow::staticMetaObject = {
+    { &QMainWindow::staticMetaObject, qt_meta_stringdata_MainWindow.data,
+      qt_meta_data_MainWindow,  qt_static_metacall, Q_NULLPTR, Q_NULLPTR}
+};
+
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return Q_NULLPTR;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata))
+        return static_cast<void*>(const_cast< MainWindow*>(this));
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 9)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 9;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 9)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 9;
+    }
+    return _id;
+}
+QT_END_MOC_NAMESPACE
---
+++ build-Stadium_Trip-Desktop_Qt_5_4_0_MSVC2013_64bit-Debug/ui_mainwindow.h
@@ -0,0 +1,303 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.4.0
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QComboBox>
+#include <QtWidgets/QDateEdit>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStackedWidget>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QToolBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralWidget;
+    QStackedWidget *stackedWidget;
+    QWidget *mainPage;
+    QLabel *ballGame_image;
+    QPushButton *quitBtn;
+    QPushButton *logInBtn;
+    QLabel *adminLogInLabel;
+    QPushButton *shoppingCartBtn;
+    QPushButton *viewStadiumBtn;
+    QPushButton *planVacationBtn;
+    QWidget *page;
+    QWidget *modifySouvenirPage;
+    QPushButton *modifySouvenirBtn;
+    QPushButton *addSouvenirBtn;
+    QPushButton *deleteSourvenirBtn;
+    QPushButton *pushButton;
+    QWidget *adminPage;
+    QPushButton *modifySouvenirListBtn;
+    QPushButton *modifyStadiumListBtn;
+    QPushButton *pushButton_2;
+    QWidget *addStadiumPage;
+    QLineEdit *stadiumNameBox;
+    QLabel *NameLabel;
+    QLabel *teamNameLabel;
+    QLineEdit *teamNameBox;
+    QLabel *addressLabel;
+    QLineEdit *addressBox;
+    QLabel *phoneLabel;
+    QLineEdit *phoineBox;
+    QLabel *leagueLabel;
+    QComboBox *leagueComboBox;
+    QLabel *dateLabel;
+    QDateEdit *dateEdit;
+    QLabel *grassLabel;
+    QComboBox *grassComboBox;
+    QWidget *modifyStadiumPage;
+    QPushButton *modifyStadiumBtn;
+    QPushButton *addStadiumBtn;
+    QPushButton *pushButton_3;
+    QWidget *planVacationPage;
+    QPushButton *shortestAngelBtn;
+    QPushButton *customizeShortestBtn;
+    QPushButton *MSTBtn;
+    QPushButton *pushButton_4;
+    QMenuBar *menuBar;
+    QToolBar *mainToolBar;
+    QStatusBar *statusBar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QStringLiteral("MainWindow"));
+        MainWindow->resize(643, 505);
+        MainWindow->setAcceptDrops(true);
+        centralWidget = new QWidget(MainWindow);
+        centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        stackedWidget = new QStackedWidget(centralWidget);
+        stackedWidget->setObjectName(QStringLiteral("stackedWidget"));
+        stackedWidget->setGeometry(QRect(0, -1, 641, 451));
+        mainPage = new QWidget();
+        mainPage->setObjectName(QStringLiteral("mainPage"));
+        ballGame_image = new QLabel(mainPage);
+        ballGame_image->setObjectName(QStringLiteral("ballGame_image"));
+        ballGame_image->setGeometry(QRect(220, 20, 200, 200));
+        QSizePolicy sizePolicy(QSizePolicy::Ignored, QSizePolicy::Ignored);
+        sizePolicy.setHorizontalStretch(0);
+        sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(ballGame_image->sizePolicy().hasHeightForWidth());
+        ballGame_image->setSizePolicy(sizePolicy);
+        ballGame_image->setAlignment(Qt::AlignCenter);
+        quitBtn = new QPushButton(mainPage);
+        quitBtn->setObjectName(QStringLiteral("quitBtn"));
+        quitBtn->setGeometry(QRect(250, 390, 150, 41));
+        logInBtn = new QPushButton(mainPage);
+        logInBtn->setObjectName(QStringLiteral("logInBtn"));
+        logInBtn->setGeometry(QRect(520, 370, 75, 23));
+        adminLogInLabel = new QLabel(mainPage);
+        adminLogInLabel->setObjectName(QStringLiteral("adminLogInLabel"));
+        adminLogInLabel->setGeometry(QRect(520, 350, 101, 16));
+        shoppingCartBtn = new QPushButton(mainPage);
+        shoppingCartBtn->setObjectName(QStringLiteral("shoppingCartBtn"));
+        shoppingCartBtn->setGeometry(QRect(250, 340, 150, 41));
+        viewStadiumBtn = new QPushButton(mainPage);
+        viewStadiumBtn->setObjectName(QStringLiteral("viewStadiumBtn"));
+        viewStadiumBtn->setGeometry(QRect(250, 290, 150, 41));
+        planVacationBtn = new QPushButton(mainPage);
+        planVacationBtn->setObjectName(QStringLiteral("planVacationBtn"));
+        planVacationBtn->setGeometry(QRect(250, 240, 150, 41));
+        stackedWidget->addWidget(mainPage);
+        page = new QWidget();
+        page->setObjectName(QStringLiteral("page"));
+        stackedWidget->addWidget(page);
+        modifySouvenirPage = new QWidget();
+        modifySouvenirPage->setObjectName(QStringLiteral("modifySouvenirPage"));
+        modifySouvenirBtn = new QPushButton(modifySouvenirPage);
+        modifySouvenirBtn->setObjectName(QStringLiteral("modifySouvenirBtn"));
+        modifySouvenirBtn->setGeometry(QRect(209, 220, 181, 41));
+        addSouvenirBtn = new QPushButton(modifySouvenirPage);
+        addSouvenirBtn->setObjectName(QStringLiteral("addSouvenirBtn"));
+        addSouvenirBtn->setGeometry(QRect(210, 170, 181, 41));
+        addSouvenirBtn->setAcceptDrops(false);
+        deleteSourvenirBtn = new QPushButton(modifySouvenirPage);
+        deleteSourvenirBtn->setObjectName(QStringLiteral("deleteSourvenirBtn"));
+        deleteSourvenirBtn->setGeometry(QRect(210, 270, 181, 41));
+        deleteSourvenirBtn->setAcceptDrops(false);
+        pushButton = new QPushButton(modifySouvenirPage);
+        pushButton->setObjectName(QStringLiteral("pushButton"));
+        pushButton->setGeometry(QRect(490, 400, 75, 23));
+        stackedWidget->addWidget(modifySouvenirPage);
+        adminPage = new QWidget();
+        adminPage->setObjectName(QStringLiteral("adminPage"));
+        modifySouvenirListBtn = new QPushButton(adminPage);
+        modifySouvenirListBtn->setObjectName(QStringLiteral("modifySouvenirListBtn"));
+        modifySouvenirListBtn->setGeometry(QRect(219, 230, 181, 41));
+        modifyStadiumListBtn = new QPushButton(adminPage);
+        modifyStadiumListBtn->setObjectName(QStringLiteral("modifyStadiumListBtn"));
+        modifyStadiumListBtn->setGeometry(QRect(220, 180, 181, 41));
+        modifyStadiumListBtn->setAcceptDrops(false);
+        pushButton_2 = new QPushButton(adminPage);
+        pushButton_2->setObjectName(QStringLiteral("pushButton_2"));
+        pushButton_2->setGeometry(QRect(510, 420, 75, 23));
+        stackedWidget->addWidget(adminPage);
+        addStadiumPage = new QWidget();
+        addStadiumPage->setObjectName(QStringLiteral("addStadiumPage"));
+        stadiumNameBox = new QLineEdit(addStadiumPage);
+        stadiumNameBox->setObjectName(QStringLiteral("stadiumNameBox"));
+        stadiumNameBox->setGeometry(QRect(327, 85, 113, 20));
+        NameLabel = new QLabel(addStadiumPage);
+        NameLabel->setObjectName(QStringLiteral("NameLabel"));
+        NameLabel->setGeometry(QRect(187, 85, 131, 16));
+        teamNameLabel = new QLabel(addStadiumPage);
+        teamNameLabel->setObjectName(QStringLiteral("teamNameLabel"));
+        teamNameLabel->setGeometry(QRect(206, 118, 111, 16));
+        teamNameBox = new QLineEdit(addStadiumPage);
+        teamNameBox->setObjectName(QStringLiteral("teamNameBox"));
+        teamNameBox->setGeometry(QRect(328, 117, 113, 20));
+        addressLabel = new QLabel(addStadiumPage);
+        addressLabel->setObjectName(QStringLiteral("addressLabel"));
+        addressLabel->setGeometry(QRect(226, 156, 91, 16));
+        addressBox = new QLineEdit(addStadiumPage);
+        addressBox->setObjectName(QStringLiteral("addressBox"));
+        addressBox->setGeometry(QRect(329, 155, 113, 20));
+        phoneLabel = new QLabel(addStadiumPage);
+        phoneLabel->setObjectName(QStringLiteral("phoneLabel"));
+        phoneLabel->setGeometry(QRect(191, 191, 121, 16));
+        phoineBox = new QLineEdit(addStadiumPage);
+        phoineBox->setObjectName(QStringLiteral("phoineBox"));
+        phoineBox->setGeometry(QRect(329, 192, 113, 20));
+        leagueLabel = new QLabel(addStadiumPage);
+        leagueLabel->setObjectName(QStringLiteral("leagueLabel"));
+        leagueLabel->setGeometry(QRect(222, 232, 91, 16));
+        leagueComboBox = new QComboBox(addStadiumPage);
+        leagueComboBox->setObjectName(QStringLiteral("leagueComboBox"));
+        leagueComboBox->setGeometry(QRect(329, 230, 41, 22));
+        dateLabel = new QLabel(addStadiumPage);
+        dateLabel->setObjectName(QStringLiteral("dateLabel"));
+        dateLabel->setGeometry(QRect(203, 306, 111, 16));
+        dateEdit = new QDateEdit(addStadiumPage);
+        dateEdit->setObjectName(QStringLiteral("dateEdit"));
+        dateEdit->setGeometry(QRect(329, 303, 110, 22));
+        dateEdit->setMaximumDateTime(QDateTime(QDate(2015, 5, 17), QTime(23, 59, 59)));
+        dateEdit->setMinimumDateTime(QDateTime(QDate(1800, 1, 1), QTime(0, 0, 0)));
+        grassLabel = new QLabel(addStadiumPage);
+        grassLabel->setObjectName(QStringLiteral("grassLabel"));
+        grassLabel->setGeometry(QRect(258, 272, 61, 16));
+        grassComboBox = new QComboBox(addStadiumPage);
+        grassComboBox->setObjectName(QStringLiteral("grassComboBox"));
+        grassComboBox->setGeometry(QRect(329, 269, 41, 22));
+        stackedWidget->addWidget(addStadiumPage);
+        modifyStadiumPage = new QWidget();
+        modifyStadiumPage->setObjectName(QStringLiteral("modifyStadiumPage"));
+        modifyStadiumBtn = new QPushButton(modifyStadiumPage);
+        modifyStadiumBtn->setObjectName(QStringLiteral("modifyStadiumBtn"));
+        modifyStadiumBtn->setGeometry(QRect(220, 240, 181, 41));
+        addStadiumBtn = new QPushButton(modifyStadiumPage);
+        addStadiumBtn->setObjectName(QStringLiteral("addStadiumBtn"));
+        addStadiumBtn->setGeometry(QRect(221, 190, 181, 41));
+        addStadiumBtn->setAcceptDrops(false);
+        pushButton_3 = new QPushButton(modifyStadiumPage);
+        pushButton_3->setObjectName(QStringLiteral("pushButton_3"));
+        pushButton_3->setGeometry(QRect(490, 410, 75, 23));
+        stackedWidget->addWidget(modifyStadiumPage);
+        planVacationPage = new QWidget();
+        planVacationPage->setObjectName(QStringLiteral("planVacationPage"));
+        shortestAngelBtn = new QPushButton(planVacationPage);
+        shortestAngelBtn->setObjectName(QStringLiteral("shortestAngelBtn"));
+        shortestAngelBtn->setGeometry(QRect(210, 180, 181, 41));
+        shortestAngelBtn->setAcceptDrops(false);
+        customizeShortestBtn = new QPushButton(planVacationPage);
+        customizeShortestBtn->setObjectName(QStringLiteral("customizeShortestBtn"));
+        customizeShortestBtn->setGeometry(QRect(209, 230, 181, 41));
+        MSTBtn = new QPushButton(planVacationPage);
+        MSTBtn->setObjectName(QStringLiteral("MSTBtn"));
+        MSTBtn->setGeometry(QRect(209, 280, 181, 41));
+        pushButton_4 = new QPushButton(planVacationPage);
+        pushButton_4->setObjectName(QStringLiteral("pushButton_4"));
+        pushButton_4->setGeometry(QRect(490, 420, 75, 23));
+        stackedWidget->addWidget(planVacationPage);
+        MainWindow->setCentralWidget(centralWidget);
+        menuBar = new QMenuBar(MainWindow);
+        menuBar->setObjectName(QStringLiteral("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 643, 21));
+        MainWindow->setMenuBar(menuBar);
+        mainToolBar = new QToolBar(MainWindow);
+        mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
+        MainWindow->addToolBar(Qt::TopToolBarArea, mainToolBar);
+        statusBar = new QStatusBar(MainWindow);
+        statusBar->setObjectName(QStringLiteral("statusBar"));
+        MainWindow->setStatusBar(statusBar);
+
+        retranslateUi(MainWindow);
+
+        stackedWidget->setCurrentIndex(0);
+
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", 0));
+        ballGame_image->setText(QApplication::translate("MainWindow", "TextLabel", 0));
+        quitBtn->setText(QApplication::translate("MainWindow", "Quit", 0));
+        logInBtn->setText(QApplication::translate("MainWindow", "Log in", 0));
+        adminLogInLabel->setText(QApplication::translate("MainWindow", "Administrator Log In", 0));
+        shoppingCartBtn->setText(QApplication::translate("MainWindow", "Shopping Cart", 0));
+        viewStadiumBtn->setText(QApplication::translate("MainWindow", "View Stadiums", 0));
+        planVacationBtn->setText(QApplication::translate("MainWindow", "Plan A Vacation", 0));
+        modifySouvenirBtn->setText(QApplication::translate("MainWindow", "Modify Souvenir", 0));
+        addSouvenirBtn->setText(QApplication::translate("MainWindow", "Add Souvenir", 0));
+        deleteSourvenirBtn->setText(QApplication::translate("MainWindow", "Delete Souvenir", 0));
+        pushButton->setText(QApplication::translate("MainWindow", "Back", 0));
+        modifySouvenirListBtn->setText(QApplication::translate("MainWindow", "Modify Souvenir List", 0));
+        modifyStadiumListBtn->setText(QApplication::translate("MainWindow", "Modify Stadium List", 0));
+        pushButton_2->setText(QApplication::translate("MainWindow", "Back", 0));
+        NameLabel->setText(QApplication::translate("MainWindow", "Enter the Stadiums Name:", 0));
+        teamNameLabel->setText(QApplication::translate("MainWindow", "Enter the team Name:", 0));
+        addressLabel->setText(QApplication::translate("MainWindow", "Enter the Adress:", 0));
+        phoneLabel->setText(QApplication::translate("MainWindow", "Enter the Phone number:", 0));
+        leagueLabel->setText(QApplication::translate("MainWindow", "Select the League:", 0));
+        leagueComboBox->clear();
+        leagueComboBox->insertItems(0, QStringList()
+         << QApplication::translate("MainWindow", "NL", 0)
+         << QApplication::translate("MainWindow", "AL", 0)
+        );
+        dateLabel->setText(QApplication::translate("MainWindow", "set the date it opened:", 0));
+        grassLabel->setText(QApplication::translate("MainWindow", "Grass field:", 0));
+        grassComboBox->clear();
+        grassComboBox->insertItems(0, QStringList()
+         << QApplication::translate("MainWindow", "Yes", 0)
+         << QApplication::translate("MainWindow", "No", 0)
+        );
+        modifyStadiumBtn->setText(QApplication::translate("MainWindow", "Modify Stadium", 0));
+        addStadiumBtn->setText(QApplication::translate("MainWindow", "Add Stadium", 0));
+        pushButton_3->setText(QApplication::translate("MainWindow", "Back", 0));
+        shortestAngelBtn->setText(QApplication::translate("MainWindow", "Shortest Trip from Angel Stadium", 0));
+        customizeShortestBtn->setText(QApplication::translate("MainWindow", "Customize Shortest Trip", 0));
+        MSTBtn->setText(QApplication::translate("MainWindow", "Minimum Spanning Tree", 0));
+        pushButton_4->setText(QApplication::translate("MainWindow", "Back", 0));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
